9e8b8840795cc3a5c675f01aa3ace04b78dd75e1
xonix <xonixx@gmail.com>
2019-07-28 15:22:36 +0300

    service_1 bootstrapped from spring initializr

---
+++ service_1/src/main/java/com/github/xonixx/sample_app_1/service_1/Service1Application.java
@@ -0,0 +1,13 @@
+package com.github.xonixx.sample_app_1.service_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Service1Application.class, args);
+	}
+
+}
