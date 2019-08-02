fab3646b87f11ecdc5d706f425bf31acbc054f19
xonix <xonixx@gmail.com>
2019-08-02 04:11:50 +0300

    attempt to build with Docker in Jenkins

--- service_1/deploy_k8s.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-GIT_REVISION=$1
-
-kubectl run service_1 --image=xonixx/service_1:$GIT_REVISION --replicas=1 --port=34701
-
-kubectl expose deployment service_1 --port=34701 --type=LoadBalancer
